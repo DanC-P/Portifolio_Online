98c9aa57cc05346722ab5895e09128c945ca319e
DanC-P <danielsantoscosta18@hotmail.com>
2026-05-22 12:46:21 -0300

    feat: criar componente header

--- portfolio-online/src/app/app.ts
+++ portfolio-online/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarApp } from './components/sidebar-app/sidebar-app';
+import { HeaderApp } from './components/header-app/header-app';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarApp],
+  imports: [SidebarApp, HeaderApp],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ portfolio-online/src/app/components/header-app/header-app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-app',
+  imports: [],
+  templateUrl: './header-app.html',
+  styleUrl: './header-app.css',
+})
+export class HeaderApp {}

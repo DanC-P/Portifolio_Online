7fb722ac06d493b575b90ed281ca5595d8f2a245
DanC-P <danielsantoscosta18@hotmail.com>
2026-05-23 10:41:12 -0300

    feat: adicionar componente footer

---
+++ portfolio-online/src/app/components/footer-app/footer-app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-app',
+  imports: [],
+  templateUrl: './footer-app.html',
+  styleUrl: './footer-app.css',
+})
+export class FooterApp {}
--- portfolio-online/src/app/pages/home-page/home-page.ts
+++ portfolio-online/src/app/pages/home-page/home-page.ts
@@ -5,10 +5,11 @@ import { SkillsApp } from '../../components/skills-app/skills-app';
 import { LanguagesApp } from '../../components/languages-app/languages-app';
 import { ToolsApp } from '../../components/tools-app/tools-app';
 import { ProjectsApp } from '../../components/projects-app/projects-app';
+import { FooterApp } from '../../components/footer-app/footer-app';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderApp, SkillsApp, LanguagesApp, ToolsApp, ProjectsApp],
+  imports: [HeaderApp, SkillsApp, LanguagesApp, ToolsApp, ProjectsApp, FooterApp],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
